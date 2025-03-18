6af30e9b8b993abc601a12664c1043723200a22b
Expert Snippet <gunique885@gmail.com>
2025-03-18 07:26:40 +0530

    third commit

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -17,9 +17,9 @@ const Footer = ({isDarkMode}) => {
     <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
         <p>© 2025 Expert Snippet. All rights reserved.</p>
         <ul className='flex items-center gap-10 justify-center mt-4 sm:mt-0'>
-            <li><a target='_blank' href="https://instagram.com/greatstackdev">GitHub</a></li>
-            <li><a target='_blank' href="https://instagram.com/greatstackdev">LinkedIn</a></li>
-            <li><a target='_blank' href="https://instagram.com/greatstackdev">Twitter</a></li>
+            <li><a target='_blank' href="https://www.instagram.com/expert.snippet/">Instagram</a></li>
+            <li><a target='_blank' href="https://www.facebook.com/profile.php?id=61572854462412">FaceBook</a></li>
+            <li><a target='_blank' href="https://x.com/expertsnippet">Twitter</a></li>
         </ul>
     </div>
 
