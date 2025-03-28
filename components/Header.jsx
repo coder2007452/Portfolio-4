64adb17056477ccc7967601b9c0adcdeee3d8b20
Expert Snippet <gunique885@gmail.com>
2025-03-29 03:49:25 +0530

    002

--- components/Header.jsx
+++ components/Header.jsx
@@ -24,14 +24,14 @@ const Header = () => {
     whileInView={{y: 0, opacity: 1}}
     transition={{duration: 0.8, delay: 0.5}}
     className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>
-        studing web development based in India.</motion.h1>
+        studying web development based in India.</motion.h1>
 
         <motion.p
         initial={{opacity: 0}}
         whileInView={{opacity: 1}}
         transition={{duration: 0.6, delay: 0.7}}
         className='max-w-2xl mx-auto font-Ovo'>
-        I am studing web development from Gujarat, India.
+        I am studying web development from Gujarat, India.
         </motion.p>
 
         <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
